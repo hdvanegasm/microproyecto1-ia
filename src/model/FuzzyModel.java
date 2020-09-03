bcdffb84579586dd669510e15493adb03fb02238
hernan232 <hdvanegasm@gmail.com>
2020-09-02 19:29:22 -0500

    Added .fcl file

--- src/model/FuzzyModel.java
+++ src/model/FuzzyModel.java
@@ -11,13 +11,17 @@ package model;
  */
 
 import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.rule.Rule;
+
+import java.util.List;
 
 public class FuzzyModel {
     private static FIS model;
     
     public static FIS getModel() {
         if(model == null) {
-            String filePath = "model.fcl";
+            // TODO Implement .fcl
+            String filePath = "src/model/apple_model.fcl";
             model = FIS.load(filePath);
             return model;
         } else {
@@ -25,7 +29,30 @@ public class FuzzyModel {
         }
     }
     
-    public static double getQuality() {
-        return 0;
+    private static FIS evaluateModel(
+            double size, double redLevel, double spots) {
+        // TODO fix variable names
+        FIS fuzzyModel = getModel();
+        fuzzyModel.setVariable("size", size);
+        fuzzyModel.setVariable("red_level", redLevel);
+        fuzzyModel.setVariable("spots", spots);
+        
+        return fuzzyModel;
+    }
+    
+    public static double getQuality(
+            double size, double redLevel, double spots) {
+        FIS fuzzyModel = evaluateModel(size, redLevel, spots);
+        return fuzzyModel.getVariable("quality").getLatestDefuzzifiedValue();
+    }
+    
+    public static List<Rule> getRules(
+            double size, double redLevel, double spots) {
+        FIS fuzzyModel = evaluateModel(size, redLevel, spots);
+        
+        return fuzzyModel
+                .getFunctionBlock("quality")
+                .getFuzzyRuleBlock("No1")
+                .getRules();
     }
 }
